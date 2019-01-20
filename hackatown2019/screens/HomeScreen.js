b36a59b6a1b3eb79647d479eb00aa2ede1499630
Alec Adub <adub.alec@gmail.com>
2019-01-19 21:58:54 -0500

    Put changes inside branch
    re

--- hackatown2019/screens/HomeScreen.js
+++ hackatown2019/screens/HomeScreen.js
@@ -8,25 +8,95 @@ import {
     Text,
     TouchableOpacity,
     View,
+    Dimensions
 } from 'react-native';
 
+const {width, height} = Dimensions.get('window');
+
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 1
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-        header: null,
-    };
+    constructor(props){
+        super(props);
+
+        this.state = {
+            initialPosition: {
+                latitude: 0,
+                longitude: 0,
+                latitudeDelta: 0,
+                longitudeDelta: 0
+            },
+            markerPosition: {
+                latitude: 0,
+                longitude: 0
+            },
+            polyPosition: {
+                latitude: 45.504384,
+                longitude: -73.612883
+            }
+        }
+    }
+
+    watchID: ?number = null;
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(position => {
+            var lat = parseFloat(position.coords.latitude)
+            var long = parseFloat(position.coords.longitude)
+
+            var initialRegion = {
+                latitude: lat,
+                longitude: long,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA
+            }
+
+            this.setState({initialPosition: initialRegion})
+            this.setState({markerPosition: initialRegion})
+        }, (error) => alert(JSON.stringify(error)),{enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+        this.watchID = navigator.geolocation.watchPosition(position => {
+            var lat = parseFloat(position.coords.latitude)
+            var long = parseFloat(position.coords.longitude)
+
+            var lastRegion = {
+                latitude: lat,
+                longitude: long,
+                longitudeDelta: LONGITUDE_DELTA,
+                latitudeDelta: LATITUDE_DELTA
+            }
+
+            this.setState({initialPosition: lastRegion})
+            this.setState({markerPosition: lastRegion})
+
+        })
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchID)
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <MapView style={styles.map}
-                         region={{
-                             latitude: 60,
-                             longitude: 18,
-                             latitudeDelta: 0.1,
-                             longitudeDelta: 0.1
-                         }}
+                         region={this.state.initialPosition}
                 >
+                    <MapView.Marker
+                        coordinate={this.state.markerPosition}>
+                        <View style = {styles.radius}>
+                            <View style = {styles.marker}>
+
+                            </View>
+                        </View>
+                    </MapView.Marker>
+                    <MapView.Marker coordinate={this.state.polyPosition}/>
                 </MapView>
+
 
             </View>
         );
@@ -34,6 +104,7 @@ export default class HomeScreen extends React.Component {
 
 
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -51,6 +122,26 @@ const styles = StyleSheet.create({
         left:0,
         bottom: 0,
         right: 0
+    },
+    radius: {
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(0, 112, 255, 0.1)',
+        borderWidth: 1,
+        borderColor: 'rgba(0, 112, 255, 0.1',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    marker: {
+        height:20,
+        width: 20,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 20/2,
+        overflow: 'hidden',
+        backgroundColor: '#007AFF'
     }
 
 });
